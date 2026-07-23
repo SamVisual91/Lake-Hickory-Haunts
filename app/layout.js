5638429e5c9ce7d3498b4098b3c61c797fdf92e3
Sam Thao <contactme@samthao.com>
2026-07-23 11:43:34 -0400

    Add GTM tracking and refine VIP panel styling

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,12 @@
 import "./globals.css";
+import Script from "next/script";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import { HistoryNavigationReloader } from "../components/HistoryNavigationReloader";
 import { DripReviewsWidget } from "../components/DripReviewsWidget";
 import { DEFAULT_DESCRIPTION, SITE_NAME, SITE_URL } from "../lib/seo";
+
+const GTM_ID = "GTM-WL2RMD";
 
 const localBusinessSchema = {
   "@context": "https://schema.org",
@@ -55,6 +58,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessSchema) }}
@@ -70,4 +90,3 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-
